97da0015e8388e734e8ae5e3ac0b6abb523f6782
Michał Krzysztof Zygmunt Feiler <feiler.michal@gmail.com>
2014-05-30 16:03:44 +0200

    Dodano zapetlony.py oraz demon.sh

---
+++ demon.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+i=0
+while :
+do
+	i=$[i + 1]
+	echo "Zatoczenie petli nr $i"
+	rm -v 0*TOR.js*
+	for j in 01 02 03 04 05 06 07 08 09 10 11 12
+	do
+		wget -q --no-check-certificate trm24.pl/panel-trm/0${j}TOR.jsp && echo "Pobieranie stacji 0${j}TOR sukcesem zakonczone"
+		python zapetlony.py 0${j}TOR.jsp 0${j}TOR
+	done
+	echo "Zatoczenie $i completed"
+done
+echo "Zakonczenie programu przez dziwny traf"
+exit
