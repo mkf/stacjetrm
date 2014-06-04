ef72dc1b77b39d6e7bd84f6272f5bf165c690acc
Michał Krzysztof Zygmunt Feiler <feiler.michal@gmail.com>
2014-06-04 18:49:21 +0200

    Pierwsze kroki ku stworzeniu nowego labormodo: a, jak adres – rozszerzenia 'l' posiadającego adres

--- orzeczim.py
+++ orzeczim.py
@@ -13,16 +13,16 @@ class orzeczim:
 		if row == 0:
 			self.wyd = "estas neniu bicikloj"
 		elif row == -1:
-			self.wyd = "okazis iun eraron, cxar gxi transdonas ke tie estas -1 biciklo - kaj tio estas negativa kvanto"
+			self.wyd = "okazis iun eraron, cxar gxi transdonas ke tie estas -1 biciklo - kaj tio estas negativa kvanto \n         "
 		elif row <= 0:
-			self.wyd = "okazis iun eraron, cxar gxi transdonas ke tie estas " + str(row) + " bicikloj - kaj tio estas negativa kvanto"
+			self.wyd = "okazis iun eraron, cxar gxi transdonas ke tie estas " + str(row) + " bicikloj - kaj tio estas negativa kvanto \n          "
 		elif row == 1:
-			self.wyd = "estas 1  biciklo"
+			self.wyd = "estas 1  biciklo    "
 		elif row >= 2:
 			if row <= 9:
-				self.wyd = ("estas " + str(row) + "  bicikloj")
+				self.wyd = ("estas " + str(row) + "  bicikloj   ")
 			if row >= 10:
-				self.wyd = ("estas " + str(row) + " bicikloj")
+				self.wyd = ("estas " + str(row) + " bicikloj   ")
 		else:
 			self.wyd = ", ni ne scias kiom bicikloj estas tie, cxar dum la akiroprovon de la biciklokvanto okazis la eraro"
 		wyd = self.wyd
@@ -54,4 +54,11 @@ class orzeczim:
 		st = self.stac
 		row = self.row
 		print unx, st, row
-
+	def pisa(self):
+		wyd = self.wyd
+		cz = self.cz
+		dz = self.dz
+		st = self.stac
+		row = self.row
+		ad = {'001TOR': 'Rynek Staromiejski', '002TOR': 'Plac sw. Katarzyny', '003TOR': 'Plac Rapackiego', '004TOR': 'ul. Bulwar Filadelfijski - Brama Klasztorna', '005TOR': 'ul. Szosa Chelminska - Targowisko Miejskie', '006TOR': 'ul. Gagarina - Biblioteka Uniwersytecka', '007TOR': 'ul. Broniewskiego - Tesco', '008TOR': 'ul. Gen. Jozefa Hallera - Polo Market', '009TOR': 'ul. Szosa Chelminska - Polo Market', '010TOR': 'PKP Torun Glowny', '011TOR': 'ul. Dziewulskiego - Komisariat Policji', '012TOR': 'ul. Konstytucji 3 Maja - Pawilon Maciej'}
+		print dz, cz, "Al la biciklastacion", st, wyd, " - ", ad[st]
