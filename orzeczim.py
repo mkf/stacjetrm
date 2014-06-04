14e07ddc52691f4616a6c57dff8f4fab27d45c93
Michał Krzysztof Zygmunt Feiler <feiler.michal@gmail.com>
2014-06-04 16:25:45 +0200

    Uklasowienie, oczyszczenie i jakaś zmiana w pars

--- orzeczim.py
+++ orzeczim.py
@@ -1,50 +1,56 @@
 class orzeczim:
-    "To jest klasa orzeczenia imiennego, w trybach u i l"
-    
-    def __init__(self,row,stac,tryb,dz,cz):
-        self.row = row
-        self.stac = stac
-        self.tryb = tryb
-        self.dz = dz
-        self.cz = cz
+	"To jest klasa orzeczenia imiennego, w trybach u i l"
+	
+	def __init__(self,row,stac,unx,dz,cz):
+		self.row = row
+		self.stac = stac
+		self.unx = unx
+		self.dz = dz
+		self.cz = cz
 
-    def orz(self):
-        if row == 0:
-            self.wyd = "estas neniu bicikloj"
-        elif row == -1:
-            self.wyd = "okazis iun eraron, cxar gxi transdonas ke tie estas -1 biciklo - kaj tio estas negativa kvanto"
-        elif row <= 0:
-            self.wyd = "okazis iun eraron, cxar gxi transdonas ke tie estas " + str(row) + " bicikloj - kaj tio estas negativa kvanto"
-        elif row == 1:
-            self.wyd = "estas 1  biciklo"
-        elif row >= 2:
-            if row <= 9:
-                self.wyd = ("estas " + str(row) + "  bicikloj")
-            if row >= 10:
-                self.wyd = ("estas " + str(row) + " bicikloj")
-        else:
-            self.wyd = ", ni ne scias kiom bicikloj estas tie, cxar dum la akiroprovon de la biciklokvanto okazis la eraro"
-        wyd = self.wyd
+	def orz(self):
+		if row == 0:
+			self.wyd = "estas neniu bicikloj"
+		elif row == -1:
+			self.wyd = "okazis iun eraron, cxar gxi transdonas ke tie estas -1 biciklo - kaj tio estas negativa kvanto"
+		elif row <= 0:
+			self.wyd = "okazis iun eraron, cxar gxi transdonas ke tie estas " + str(row) + " bicikloj - kaj tio estas negativa kvanto"
+		elif row == 1:
+			self.wyd = "estas 1  biciklo"
+		elif row >= 2:
+			if row <= 9:
+				self.wyd = ("estas " + str(row) + "  bicikloj")
+			if row >= 10:
+				self.wyd = ("estas " + str(row) + " bicikloj")
+		else:
+			self.wyd = ", ni ne scias kiom bicikloj estas tie, cxar dum la akiroprovon de la biciklokvanto okazis la eraro"
+		wyd = self.wyd
 
-    def pisul(self):
-        wyd = self.wyd
-        cz = self.cz
-        dz = self.dz
-        st = self.stac
-        row = self.row
-        print dz, cz, "Al la biciklastacion", st, wyd
+	def pisul(self):
+		wyd = self.wyd
+		cz = self.cz
+		dz = self.dz
+		st = self.stac
+		row = self.row
+		print dz, cz, "Al la biciklastacion", st, wyd
 
-    def pisp(self):
-        cz = self.cz
-        dz = self.dz
-        st = self.stac
-        row = self.row
-        print dz, cz, st, row
+	def pisp(self):
+		cz = self.cz
+		dz = self.dz
+		st = self.stac
+		row = self.row
+		print dz, cz, st, row
 
-    def pisc(self):
-        cz = self.cz
-        dz = self.dz
-        st = self.stac
-        row = self.row
-        print dz, cz, st, row
+	def pisc(self):
+		import re
+		unx = self.unx
+		st = self.stac
+		stn = int(re.sub(r'\D',"",st))
+		row = self.row
+		print unx, stn, row
+	def pism(self):
+		unx = self.unx
+		st = self.stac
+		row = self.row
+		print unx, st, row
 
--- zapetlony.py
+++ zapetlony.py
@@ -1,6 +1,5 @@
 import re
 import sys
-#import pars
 from pars import *
 from orzeczim import *
 plik = sys.argv[1]
@@ -12,10 +11,6 @@ dzien = sys.argv[4]	#dzien lokalny
 czas = sys.argv[5]	#czas lokalny
 #sf = 			#save file
 #sm = 			#save mode
-slowniczekkoncowek = {'001TOR.jsp': 'jski', '002TOR.jsp': 'rzyny', '003TOR.jsp': 'packiego', '004TOR.jsp': 'torna', '005TOR.jsp': 'ejskie', '006TOR.jsp': 'sytecka', '007TOR.jsp': 'esco', '008TOR.jsp': 'arket', '009TOR.jsp': 'arket', '010TOR.jsp': 'wny', '011TOR.jsp': 'olicji', '012TOR.jsp': 'aciej'}
-# ^ to tutaj to slowniczek koncowek, dzieki ktoremu bedzie mozna wyodrebnic sama liczbe rowerow, ktora jest jedyna liczba pomiedzy owa koncowka a "szt.".
-#  i tak nie uzylem slowniczka, ale przynajmniej przypomnialem sobie dictionaries w pythonie
-szt = '.*szt'
 
 file = open(plik, 'r')
 #save = open(sf, 'a')
@@ -23,48 +18,22 @@ file = open(plik, 'r')
 par = pars(file.read(), stacyja)
 rowery = par.rowery
 stacjinazwa = stacyja
-orze = oreczim(rowery, stacjinazwa, pracy, dzien, czas)
+orze = oreczim(rowery, stacjinazwa, unix, dzien, czas)
 if pracy == 'u':
-        orze.orz()
-        orze.pisul()
+	orze.orz()
+	orze.pisul()
 elif pracy == 'l':
-        orze.orz()                                                  
-        orze.pisul()
-
-#if rowery == 0:
-#	orzim = "estas neniu bicikloj"
-#elif rowery == -1:
-#	orzim = "okazis iun eraron, cxar gxi transdonas ke tie estas -1 biciklo - kaj tio estas negativa kvanto"
-#elif rowery <= 0:
-#	orzim = "okazis iun eraron, cxar gxi transdonas ke tie estas " + str(rowery) + " bicikloj - kaj tio estas negativa kvanto"
-#elif rowery == 1:
-#	orzim = "estas 1  biciklo"
-#elif rowery >= 2:
-#	if rowery <= 9:
-#		orzim = ("estas " + str(rowery) + "  bicikloj")
-#	if rowery >= 10:
-#		orzim = ("estas " + str(rowery) + " bicikloj")
-#else:
-#	orzim = ", ni ne scias kiom bicikloj estas tie, cxar dum la akiroprovon de la biciklokvanto okazis la eraro"
-#La "if" super estos uzota pri la interfaco por homojn: modo 'l' kaj 'u'
-if pracy == 'u':
-	print dzien, czas, "Al la biciklastacio", stacjinazwa, orzim
-elif pracy == 'l':
-        print dzien, czas, "Al la biciklastacio", stacjinazwa, orzim
-elif pracy == "p":
-	print dzien, czas, stacjinazwa, rowery
-elif pracy == "c":
-	print unix, stacja, rowery
-elif pracy == "m":
-	print unix, stacjinazwa, rowery
+	orze.orz()                                                  
+	orze.pisul()
+elif pracy == 'p':
+	orze.pisp()
+elif pracy == 'c':
+	orze.pisc()
+elif pracy == 'm':
+	orze.pism()
 else:
 	print "Elektu labormodo u, l, p, c aux m se vi volas ke tiun programon funkcas."
-	print "Plej eble ke la labormodo ", pracy, " estas malbona."
-#if param1 == "p":
-#	print "
-#print "Czas: ", unix, " --  na stacji ", stacjinazwa, " jest ", rowery, " rowerow"
-#print 'Na stacji ', stacyja, ' jest ', szt, ' rowerow miejskich.'
-
+        print "Plej eble ke la labormodo ", pracy, " estas malbona."
 
 #tu bedzie jeszcze append do csvka
 #EDIT: lub innej, sensowniejszej bazy danych
