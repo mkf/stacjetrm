2547b200aed61819839e0560a50f1036753ced2f
Michał Krzysztof Zygmunt Feiler <feiler.michal@gmail.com>
2014-06-05 20:04:44 +0200

    Dodano brakujące dwukropki w spacje.py

--- spacje.py
+++ spacje.py
@@ -1,12 +1,12 @@
 class spacje:
 	"To jest klasa wypisujaca odpowiednia ilosc spacji do jednej zmiennej"
 
-	def __init__(self,sc)
+	def __init__(self,sc):
 		self.sc = sc
 		spac = " "
 		for i in range(sc):
 			spac = spac + ' '
 		self.spac = spac
-	def spaces(self)
+	def spaces(self):
 		spac = self.spac
 		return spac
