3621969d99d94d82026310b7117c63544b6f6a4a
Michał Krzysztof Zygmunt Feiler <feiler.michal@gmail.com>
2014-06-23 12:18:10 +0200

    Przerabianie c.d.

--- zapisujkolejno.py
+++ zapisujkolejno.py
@@ -1,3 +1,9 @@
 class zapisujkolejno:
 	"To jest klasa zapisujaca wszystkie stacje po kolei"
 	from zapis import *
+	
+	def __init__(self,plik,pracy,debugu):
+		self.plik = plik
+		self.pracy = pracy
+		self.debugu = debugu
+		
--- zapisujrazem.py
+++ zapisujrazem.py
@@ -1,5 +1,4 @@
 class zapisujrazem:
 	"To jest klasa zapisująca wszystkie stacje razem"
-	from dajrazem import *
-	def __init__(self,pracy,debugu,unix,dzien,czas,trwania):
-		
+	def __init__(self,plik,rowerdict,pracy,debugu,unix,dzien,czas):
+				
