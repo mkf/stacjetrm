6180f440888410f40aa121dad4123d0682193806
Michał Krzysztof Zygmunt Feiler <feiler.michal@gmail.com>
2014-06-25 12:31:57 +0200

    Dalsze prace nad plikami językowymi

---
+++ esperanto.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+class esperanto:
+	u"Tio estas la lingvesperanto"
+	
+	def __init__(self):
+		badwaittime = "Malbonan atendotempon"
+		self.badwaittime = badwaittime
+	def badlang(self):
+		return("Malbonan lingvon")
