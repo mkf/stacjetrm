1a89d7a532b41504b94b932869819ab3738ff1b6
Michał Krzysztof Zygmunt Feiler <feiler.michal@gmail.com>
2014-06-26 00:16:04 +0200

    Przerzucanie się na argparse

--- demonwszystkich.py
+++ demonwszystkich.py
@@ -2,11 +2,40 @@
 import sys
 import re
 import argparse
-from paramdemonwszystkich import *
-argu = sys.argv
-prmdw = paramdemonwszystkich(argu)
-lng = paramdemonwszystkich.lng()
+#from paramdemonwszystkich import *
+#argu = sys.argv
+#prmdw = paramdemonwszystkich(argu)
+#lng = paramdemonwszystkich.lng()
 argh = argparse.ArgumentParser()
+arglang = argh.add_mutually_exclusive_group()
+argtime = argh.add_mutually_exclusive_group()
+argstac = argh.add_mutually_exclusive_group()
+argpracy = argh.add_mutually_exclusive_group()
+argdebugu = argh.add_mutually_exclusive_group()
+argzapisu = argh.add_mutually_exclusive_group()
 #argh.add_argument("-l", "--lang", type=str, help="Jednoznakowy kod języka: \nOne-character language code: \nUnulitera lingvkodo: \n - a - English \n - e - Esperanto \n - p - Polski \n - d - Deutsch \n ")
-argh.add_argument("-la", "--langenglish", type=
-argh.add_argument("-t", "--time", type=int, 
+arglang.add_argument("-la", "--langenglish", action="store_true", help="LANG: English")
+arglang.add_argument("-le", "--langesperanto", action="store_true", help="LANG: Esperanto")
+arglang.add_argument("-lp", "--langpolski", action="store_true", help="LANG: Polski")
+arglang.add_argument("-ld", "--langdeutsch", action="store_true", help="LANG: Deutsch")
+argtime.add_argument("-t", "--time", type=int, help="Opóźnienie między zbieraniem danych/Atendtempo/Delay between instances")
+argtime.add_argument("-td", "--deftime", action="store_true", help="-t z domyślną wartością/-t with default value")
+argstac.add_argument("-sa", "--allstations", action="store_true", help="Wszystkie stacje/Ĉiuj biciklstacjoj/All stations")
+argstac.add_argument("-s", "--station", action="append", help="Wybierz stację, można użyć wielokrotnie")
+argpracy.add_argument("-pf", "--pracyfull", action="store", help="Interfejs pełny z przedzieleniem na pętli i adresami")
+argpracy.add_argument("-pl", "--pracylong", action="store", help="Interfejs pełny z przedzieleniem na pętli")
+argpracy.add_argument("-pu", "--pracyuser", action="store", help="Interfejs pełny ciągły")
+argpracy.add_argument("-pa", "--pracyadres", action="store", help="Interfejs pełny z adresami")
+argpracy.add_argument("-pt", "--pracytabela", action="store", help="Interfejs tabeli z adresami")
+argpracy.add_argument("-pk", "--pracykomp", action="store", help="Interfejs programowy")
+argpracy.add_argument("-pc", "--pracycompressed", action="store", help="Interfejs programowy czasUNIX,jedno- lub dwu cyfrowy numer stacji, liczba rowerów")
+argpracy.add_argument("-pm", "--pracyminim", action="store", help="Interfejs programowy minimalistyczny")
+argpracy.add_argument("-pn", "--pracynone", action="store", help="Interfejs bez danych na stdout")
+argpracy.add_argument("-pd", "--pracydef", action="store", help="Opcja domyślna trybu pracy")
+argdebugu.add_argument("-bf", "--debugfull", action="store")
+argdebugu.add_argument("-by", "--debugyes", action="store")
+argdebugu.add_argument("-bn", "--debugno", action="store")
+argdebugu.add_argument("-bd", "--debugdef", action="store")
+argzapisu.add_argument("-wc", "--writetocsv", type=str, help="Zapis do csv, wpisz nazwę pliku")
+argzapisu.add_argument("-wn", "--writenone", action="store", help="Nie zapisuj")
+parmetry = arhg.parse_args()
