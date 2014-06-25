b8d7ff7c240776ddfabfa48fdb257944b8beadc4
Michał Krzysztof Zygmunt Feiler <feiler.michal@gmail.com>
2014-06-25 12:31:17 +0200

    Dalsze prace nad przeróbką i językami

--- demonwszystkich.py
+++ demonwszystkich.py
@@ -1,9 +1,51 @@
 # -*- coding: utf-8 -*-
 import sys
+import re
 defpracy = "a"
 defdebugu = "n"
 defsm = "n"
 deft = 60
-if sys.argv[1] == "d":
-	t = deft
-elif sys.argv[1] == 
+deflang = "e"
+argu = sys.argv
+for a in range(1,argu):
+	at = argu[a]
+	if at[0] == "l":
+		try:
+			if at[1] == "e":
+				lang = "e"
+				from esperanto import *
+				lan = esperanto()
+			elif at[1] == "a":
+				lang = "a"
+				from english import *
+				lan = english()
+			elif at[1] == "p":
+				lang = "p"
+				from polski import *
+				lan = polski()
+			elif at[1] == "d":
+				lang = "d"
+				from deutsch import *
+				lan = deutsch()
+		except:
+			from esperanto import *
+			espera = esperanto()
+			from english import *
+			engli = english()
+			from polish import *
+			polsk = polski()
+			from deutsch import *
+			deuts = deutsch()
+			for badlang in ((espera.badlang()), (engli.badlang()), (polsk.badlang()), (deuts.badlang())):
+				print "%s: %s"(badlang,at[1:])
+			quit()
+	elif at[0] == "t":
+		if at[1] == "d":
+			t = deft
+		else:
+			tc = re.sub("\D","",at)
+			try:
+				t = int(tc)
+			except:
+				print "%s: %s"(lan.badwaittime,tc)
+				quit()
