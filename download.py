65891ebcbbf40a8968aa1b246bdb3ff344871e01
Michał Krzysztof Zygmunt Feiler <feiler.michal@gmail.com>
2014-07-02 10:50:23 +0200

    Dalsze prace nad download

--- download.py
+++ download.py
@@ -5,4 +5,48 @@ class download:
 	import time
 	
 	def __init__(self,stacja):
-		
+		if stacja == 0:
+			from suma import *
+			print "Suma jeszcze nie obsługiwana wszystkiego. Exiting."
+			quit()
+		elif stacja == 100:
+			from suma import *
+			print "Suma jeszcze nie obsługiwana wybranych. Exiting."
+			quit()
+		elif stacja < 10:
+			if stacja > 0:
+				na = (r'00', str(stacja), r'TOR')
+			elif stacja < 0:
+				print "Eraro: malkorekta biciklstacio: %s" % str(stacja)
+				quit()
+		elif stacja > 9:
+			if stacja > 12:
+				na = (r'0', str(stacja), r'TOR')
+			else:
+				print "Eraro: malkorekta biciklstacio: %s" % str(stacja)
+				quit()
+		else:
+			print "Eraro: malkorekta biciklstacio: %s" % str(stacja)
+			quit()
+		stacn = str.join()
+		urlt = (r'trm24.pl/panel-trm/', str(stacn), r'.jsp')
+		url = str.join(urlt)
+		#wejs = urllib2.urlopen(url)
+		#we = wejs.read()
+		self.url = url
+		self.stacn = stacn
+	def down(self):
+		import urllib2
+		import time
+		wejs = urllib2.urlopen(self.url)
+		czas = time.time()
+		self.czas = czas
+		we = wejs.read()
+		self.we = we
+	def row(self):
+		from pars import *
+		par = pars(self.we,self.stacn)
+		rowery = par.rowerry()
+		return rowery
+	def cza(self):
+		return self.czas			
