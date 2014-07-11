378b3f29c82128813aba4e13589a7e26281fbb16
Michał Krzysztof Zygmunt Feiler <feiler.michal@gmail.com>
2014-07-12 00:34:17 +0200

    Dalsze prace nad pokazem pojedyńczym

--- pokaz.py
+++ pokaz.py
@@ -11,19 +11,19 @@ class pokaz:
 		elif pracy == 'l':
 			orze.orz()                                                  
 			orze.pisul()
-		elif pracy == 'p':
+		elif pracy == 'p' or pracy == 'k':
 			orze.pisp()
 		elif pracy == 'c':
 			orze.pisc()
 		elif pracy == 'm':
 			orze.pism()
-		elif pracy == 'a':
+		elif pracy == 'a' or pracy == 'f':
 			orze.orz()
 			orze.pisa()
 		elif pracy == 't':
 			orze.pist()
 		else:
-			print "Elektu labormodo a, l, u, p, c aux m se vi volas ke tiun programon funkcas."
+			print "Elektu labormodo 'a', 'l', 'u', 'p', 'c', 'k', 'f', 'm' aux 't' se vi volas ke tiun programon funkcas."
 			print "Plej eble ke la labormodo ", pracy, " estas malbona."
 
 	
