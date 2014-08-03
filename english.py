e177ad8922b66558bbc3abb618ac44d82b1969e2
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-08-03 21:05:07 +0200

    Dodano pliki językowe, tworzone już wcześniej, ale zapomniałem git add

---
+++ english.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+class english:
+	u"This is an English language file"
+	
+	def __init__(self):
+		lang = "en"
+	def dictu(self):
+		slo = {
+			"badwaittime": "Incorrect waiting time",
+			"badstacparam": "Incorrect station parameter",
+			"badlang": "Incorrect language",
+			"badpracy": "Incorrect work mode",
+			"baddebugu": "Incorrect debug mode",
+			"badsm": "Incorrect write mode",
+			"nastacji": "On the station",
+			"i": "i",
+		}
+		return slo
+	def wyd(self, row):
+		if row == 0:
+			wyd = "is no bicycles"
+		elif row == -1:
+			wyd = "probably encountered an error, as it shows that there is -1 bicycle, and this is a negative count \n         "
+		elif row <= 0:
+			wyd = "probably encountered an error, as it shows that there is " + str(row) + " bicycles, and this is a negative count \n          "
+		elif row == 1:
+			wyd = "is 1  bicycle    "
+		elif row >= 2:
+			if row <= 9:
+				wyd = ("is " + str(row) + "  bicycles   ")
+			if row >= 10:
+				wyd = ("is " + str(row) + " bicycles   ")
+		else:
+			wyd = ", there is no data how many bicycles there are, because an attempt to get known encountered an error"
+		return wyd
+	def lanstac(self):
+		lanstadict = {
+			'001TOR': 'Rynek Staromiejski', 
+			'002TOR': 'Plac sw. Katarzyny', 
+			'003TOR': 'Plac Rapackiego', 
+			'004TOR': 'ul. Bulwar Filadelfijski - Brama Klasztorna', 
+			'005TOR': 'ul. Szosa Chelminska - Targowisko Miejskie', 
+			'006TOR': 'ul. Gagarina - Biblioteka Uniwersytecka', 
+			'007TOR': 'ul. Broniewskiego - Tesco', 
+			'008TOR': 'ul. Gen. Jozefa Hallera - Polo Market', 
+			'009TOR': 'ul. Szosa Chelminska - Polo Market', 
+			'010TOR': 'PKP Torun Glowny', 
+			'011TOR': 'ul. Dziewulskiego - Komisariat Policji', 
+			'012TOR': 'ul. Konstytucji 3 Maja - Pawilon Maciej'
+		}
+		return lanstadict
+		#powyzsze trzeba bedzie przetlumaczyc na angielski
---
+++ polski.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+class pl-natio:
+	u"To jest plik języka polskiego"
+	
+	def __init__(self):
+		lang = "pl-natio"
+	def dictu(self):
+		slo = {
+			"badwaittime": "Niepoprawny czas czekania",
+			"badstacparam": "Niepoprawny parametr stacji",
+			"badlang": "Niepoprawny język",
+			"badpracy": "Niepoprawny tryb pracy",
+			"baddebugu": "Niepoprawny tryb debugowania",
+			"badsm": "Niepoprawny tryb zapisu",
+			"nastacji": "Na stacji",
+			"i": "i",
+		}
+		return slo
+	def wyd(self, row):
+		if row == 0:
+			wyd = "nie ma rowerów"
+		elif row == -1:
+			wyd = "najprawdopodobniej wystąpił błąd - stacja twierdzi, że jest tam -1 rower - a jest to liczba ujemna \n         "
+		elif row <= 0:
+			wyd = "najprawdopodobniej wystąpił błąd - stacja twierdzi, że jest tam " + str(row) + " rowerów - a jest to liczba ujemna \n          "
+		elif row == 1:
+			wyd = "jest 1  rower    "
+		elif row >= 2:
+			if row <= 4:
+				wyd = ("są   " + str(row) + "  rowery   ")
+			elif row >= 5:
+				wyd = ("jest " + str(row) + "  rowerów  ")
+			elif row >= 10:
+				wyd = ("jest " + str(row) + " rowerów   ")
+			elif row >= 22:
+				wyd = ("są   " + str(row) + " rowery, choć to praktycznie niemożliwe, gdyż stacja tyle nie pomieści"
+		else:
+			wyd = ", nie wiemy ile rowerów jest na tej stacji, ponieważ próba pobrania informacji zakończyła się błędem"
+		return wyd
+	def lanstac(self):
+		lanstadict = {
+			'001TOR': 'Rynek Staromiejski', 
+			'002TOR': 'Plac św. Katarzyny', 
+			'003TOR': 'Plac Rapackiego', 
+			'004TOR': 'ul. Bulwar Filadelfijski - Brama Klasztorna', 
+			'005TOR': 'ul. Szosa Chełmińska - Targowisko Miejskie', 
+			'006TOR': 'ul. Gagarina - Biblioteka Uniwersytecka', 
+			'007TOR': 'ul. Broniewskiego - Tesco', 
+			'008TOR': 'ul. Gen. Józefa Hallera - Polo Market', 
+			'009TOR': 'ul. Szosa Chełmińska - Polo Market', 
+			'010TOR': 'PKP Toruń Główny', 
+			'011TOR': 'ul. Dziewulskiego - Komisariat Policji', 
+			'012TOR': 'ul. Konstytucji 3 Maja - Pawilon Maciej',
+		}
+		return lanstadict
+class pl-safe:
+	u"To jest plik jezyka polskiego"
+	
+	def __init__(self):
+		lang = "pl-natio"
+	def dictu(self):
+		slo = {
+			"badwaittime": "Niepoprawny czas czekania",
+			"badstacparam": "Niepoprawny parametr stacji",
+			"badlang": "Niepoprawny jezyk",
+			"badpracy": "Niepoprawny tryb pracy",
+			"baddebugu": "Niepoprawny tryb debugowania",
+			"badsm": "Niepoprawny tryb zapisu",
+			"nastacji": "Na stacji",
+			"i": "i",
+		}
+		return slo
+	
+	def wyd(self, row):
+		if row == 0:
+			wyd = "nie ma rowerów"
+		elif row == -1:
+			wyd = "najprawdopodobniej wystapil blad - stacja twierdzi, ze jest tam -1 rower - a jest to liczba ujemna \n         "
+		elif row <= 0:
+			wyd = "najprawdopodobniej wystapil blad - stacja twierdzi, ze jest tam " + str(row) + " rowerow - a jest to liczba ujemna \n          "
+		elif row == 1:
+			wyd = "jest 1  rower    "
+		elif row >= 2:
+			if row <= 4:
+				wyd = ("sa   " + str(row) + "  rowery   ")
+			elif row >= 5:
+				wyd = ("jest " + str(row) + "  rowerow  ")
+			elif row >= 10:
+				wyd = ("jest " + str(row) + " rowerow   ")
+			elif row >= 22:
+				wyd = ("sa   " + str(row) + " rowery, choc to praktycznie niemozliwe, gdyz stacja tyle nie pomiesci"
+		else:
+			wyd = ", nie wiemy ile rowerow jest na tej stacji, poniewaz proba pobrania informacji zakonczyla się bledem"
+		return wyd
+	def lanstac(self):
+		lanstadict = {
+			'001TOR': 'Rynek Staromiejski', 
+			'002TOR': 'Plac sw. Katarzyny', 
+			'003TOR': 'Plac Rapackiego', 
+			'004TOR': 'ul. Bulwar Filadelfijski - Brama Klasztorna', 
+			'005TOR': 'ul. Szosa Chelminska - Targowisko Miejskie', 
+			'006TOR': 'ul. Gagarina - Biblioteka Uniwersytecka', 
+			'007TOR': 'ul. Broniewskiego - Tesco', 
+			'008TOR': 'ul. Gen. Jozefa Hallera - Polo Market', 
+			'009TOR': 'ul. Szosa Chelminska - Polo Market', 
+			'010TOR': 'PKP Torun Glowny', 
+			'011TOR': 'ul. Dziewulskiego - Komisariat Policji', 
+			'012TOR': 'ul. Konstytucji 3 Maja - Pawilon Maciej',
+		}
+		return lanstadict
