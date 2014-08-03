70781b947e2d0077c9d4037cbaa91ca79038b3da
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-08-03 20:15:08 +0200

    Prace nad okolicami kolczyraz'a, i innymi. Na przyszłość: gdy już u stabilizuję program, dalsze prace, zmiany destabilizujące trza będzie zacząć prowadzić w osobnym branchu, który będzie się merge'ować po ustabilizowaniu.

--- demonwszystkich.py
+++ demonwszystkich.py
@@ -26,6 +26,8 @@ deflang = "e"
 defget = "k"
 defadrlangczy = "l"
 defadrchar = 'l'
+defwvt = 24
+defwvc = 1000
 argh = argparse.ArgumentParser()
 arglang = argh.add_mutually_exclusive_group()
 argchar = argh.add_mutually_exclusive_group()
@@ -33,7 +35,7 @@ argtime = argh.add_mutually_exclusive_group()
 argstac = argh.add_mutually_exclusive_group()
 argpracy = argh.add_mutually_exclusive_group()
 argdebugu = argh.add_mutually_exclusive_group()
-argzapisu = argh.add_mutually_exclusive_group()
+argzapisu = argh #.add_mutually_exclusive_group()
 arggetu = argh.add_mutually_exclusive_group()
 #argh.add_argument("-l", "--lang", type=str, help="Jednoznakowy kod języka: \nOne-character language code: \nUnulitera lingvkodo: \n - a - English \n - e - Esperanto \n - p - Polski \n - d - Deutsch \n ")
 argchar.add_argument("-cs", "--charsafe", action="store_true", help='No "unsafe" national characters in language and adresses')
@@ -42,12 +44,12 @@ arglang.add_argument("-la", "--langenglish", action="store_true", help="LANG: En
 arglang.add_argument("-le", "--langesperanto", action="store_true", help="LANG: Esperanto")
 arglang.add_argument("-lp", "--langpolski", action="store_true", help="LANG: Polski")
 arglang.add_argument("-ld", "--langdeutsch", action="store_true", help="LANG: Deutsch")
-argtime.add_argument("-t", "--time", type=int, help="Opóźnienie między zbieraniem danych/Atendtempo/Delay between instances")
+argtime.add_argument("-t", "--time", type=int, help="Opóźnienie między zbieraniem danych w sekundach/Atendtempo/Delay between instances")
 argtime.add_argument("-td", "--deftime", action="store_true", help="-t z domyślną wartością/-t with default value")
 argtime.add_argument("-ts", "--singlecheck", action="store_true", help="Jednorazowe sprawdzenie/Check once")
 argstac.add_argument("-sa", "--allstations", action="store_true", help="Wszystkie stacje/Ĉiuj biciklstacjoj/All stations")
 argstac.add_argument("-sd", "--defstations", action="store_true", help="Domyślne stacje/[def] biciklstacjoj/Default stations")
-argstac.add_argument("-s", "--station", type=int, action="append", help="Wybierz stację, można użyć wielokrotnie")
+argstac.add_argument("-s", "--station", type=int, action="append", choices=range(1,13), help="Wybierz stację, można użyć wielokrotnie")
 #argpracy.add_argument("-pf", "--pracyfull", action="store_true", help="Interfejs pełny z przedzieleniem na pętli i adresami")
 argpracy.add_argument("-pfc", "--pracyfulladrlangchosen", action="store_true", help="Interfejs pełny z przedzieleniem na pętli i w wybranym języku adresami")
 argpracy.add_argument("-pfll", "--pracyfulladrlanglocalpolishalphabet", action="store_true", help="Interfejs pełny z przedzieleniem na pętli i w lokalnym(polskim) języku adresami w alfabecie polskim")
@@ -69,18 +71,44 @@ argpracy.add_argument("-pm", "--pracyminim", action="store_true", help="Interfej
 argpracy.add_argument("-prk", "--pracyrazkomp", action="store_true", help="Interfejs programowy, wszystkie stacje naraz")
 argpracy.add_argument("-pn", "--pracynone", action="store_true", help="Interfejs bez danych na stdout")
 argpracy.add_argument("-pd", "--pracydef", action="store_true", help="Opcja domyślna trybu pracy")
-argdebugu.add_argument("-bf", "--debugfull", action="store_true")
-argdebugu.add_argument("-by", "--debugyes", action="store_true")
-argdebugu.add_argument("-bn", "--debugno", action="store_true")
-argdebugu.add_argument("-bd", "--debugdef", action="store_true")
-argzapisu.add_argument("-wck", "--writetocsvkol", type=str, help="Zapis do csv (stacje kolejno, po jednej na linię - czas zapisywany dla każdego odczytu z osobna), wpisz nazwę pliku")
-argzapisu.add_argument("-wcr", "--writetocsvraz", type=str, help="Zapis do csv (wszystkie stacje naraz, w jednej linii - czas zapisywany jednorazowo dla całej serii odczytów), wpisz nazwę pliku")
-argzapisu.add_argument("-wn", "--writenone", action="store_true", help="Nie zapisuj")
+argdebugu.add_argument("-bf", "--debugfull", action="store_true", help="Debug pełny")
+argdebugu.add_argument("-by", "--debugyes", action="store_true", help="Debug częściowy")
+argdebugu.add_argument("-bn", "--debugno", action="store_true", help="Debug wyłączony")
+argdebugu.add_argument("-bd", "--debugdef", action="store_true", help="Domyślne opcje debugu")
+argzapisu.add_argument("-wcks", "--writetocsvkolsinglefile", type=str, action="append", help="Zapis do csv (stacje kolejno, po jednej na linię - czas zapisywany dla każdego odczytu z osobna), wpisz nazwę pliku, zapis do jednego pliku ciągły")
+argzapisu.add_argument("-wcrs", "--writetocsvrazsinglefile", type=str, action="append", help="Zapis do csv (wszystkie stacje naraz, w jednej linii - czas zapisywany jednorazowo dla całej serii odczytów), wpisz nazwę pliku, zapis do jednego pliku ciągły")
+argzapisu.add_argument("-wckvt", "--writetocsvkolmultitimevolumefile", action="append", type=str, nargs=2 help=str("Zapis do csv (stacje kolejno, po jednej na linię - czas zapisywany dla każdego odczytu z osobna), wpisz początek nazwy pliku i liczbę godzin (ta druga domyślnie %s); zapis wielowolumenowy wg czasu" % defwvt))
+argzapisu.add_argument("-wcrvt", "--writetocsvrazmultitimevolumefile", action="append", type=str, nargs=2 help=str("Zapis do csv (wszystkie stacje naraz, w jednej linii - czas zapisywany jednorazowo dla całej serii odczytów), wpisz początek nazwy pliku i liczbę godzin (ta druga domyślnie %s); zapis wielowolumenowy wg czasu" % defwvt))
+#argzapisu.add_argument("-wvt", "--writemultivolumetimehours", type=int, action="store_true", help="Parametr opcjonalny zapisu wielowolumenowego: czas w godzinach na jeden plik"
+argzapisu.add_argument("-wckvc", "--writetocsvkolmulticountvolumefile", type=str, action="append", nargs=2, help=str("Zapis do csv (stacje kolejno, po jednej na linię - czas zapisywany dla każdego odczytu z osobna), wpisz początek nazwy pliku i liczbę zapisów na wolumen (domyślnie %s); zapis wielowolumenowy wg ilości zapisów" % devwvc))
+argzapisu.add_argument("-wcrvc", "--writetocsvrazmulticountvolumefile", type=str, action="append", nargs=2, help=str("Zapis do csv (wszystkie stacje naraz, w jednej linii - czas zapisywany jednorazowo dla całej serii odczytów), wpisz początek nazwy pliku i liczbę zapisów na wolumen (domyślnie %s); zapis wielowolumenowy wg ilości zapisów" % devwvc))
+#argzapisu.add_argument("-wvc", "--writemultivolumeentrycount", type=int, action="store_true", help="Parametr opcjonalny zapisu wielowolumenowego: ilość wpisów na jeden plik"
+argzapisu.add_argument("-wn", "--writeno", action="store_true", help="Nie zapisuj")
 arggetu.add_argument("-gj", "--getjednoczesnie", action="store_true", help="Pobieraj jednocześnie")
 arggetu.add_argument("-gk", "--getkolejno", action="store_true", help="Pobieraj kolejno bez odstępu czasowego")
-arggetu.add_argument("-gkw", "--getkolejnowait", type=int, help="Pobieraj kolejno z odstępem czasowym")
+arggetu.add_argument("-gkw", "--getkolejnowait", type=int, help="Pobieraj kolejno z odstępem czasowym pomiędzy odczytami pojedyńczych stacji")
 arggetu.add_argument("-gd", "--getdef", action="store_true", help="Pobieraj w trybie domyślnym")
 parmetry = argh.parse_args()
+if len(parmetry.writetocsvkolsinglefile+parmetry.writetocsvkolmultitimevolumefile+parmetry.writetocsvkolmulticountvolumefile)>0:
+	writekolczyraz = 'k'
+elif len(parmetry.writetocsvrazsinglefile+parmetry.writetocsvrazmultitimevolumefile+parmetry.writetocsvrazmulticountvolumefile)>0:
+	writekolczyraz = 'r'
+elif parmetry.writeno:
+	writekolczyraz = 'n'
+else:
+	writekolczyraz = 'n'
+#if parmetry.writetocsvkolsinglefile or parmetry.writetocsvrazsinglefile:
+#	multivol = 'j'
+#elif parmetry.writetocsvrazmultitimevolumefile or parmetry.writetocsvkolmultitimevolumefile:
+#	multivol = 't'
+#elif parmetry.writetocsvrazmulticountvolumefile or parmetry.writetocsvkolmulticountvolumefile:
+#	multivol = 'c'
+#else:
+#	multivol = 'n'
+tybyzapisu = []
+for tybzapisu in (parmetry.writetocsvkolsinglefile, parmetry.writetocsvrazsinglefile, parmetry.writetocsvrazmultitimevolumefile, parmetry.writetocsvkolmultitimevolumefile, parmetry.writetocsvrazmulticountvolumefile, parmetry.writetocsvkolmulticountvolumefile):
+	if len(tybzapisu)>0:
+		tybyzapisu.append(tybzapisu)
 if parmetry.charsafe:
 	lanchar = 'n'
 elif parmetry.charwithnational:
@@ -270,14 +298,20 @@ import time
 if kolczyraz == 'k':
 	smartget = 'k'
 elif kolczyraz == 'r':
-	smartget = 'j'
+	if writekolczyraz == 'k':
+		smartget = 'k'
+	elif writekolczyraz == 'j' or writekolczyraz == 'n':
+		smartget = 'j'
 elif kolczyraz == 'n':
-	smartget = defget
+	if writekolczyraz == 'k' or writekolczyraz == 'r':
+		smartget = writekolczyraz
+	else:
+		smartget = defget
 #               ^— to może budzić kontrowersje. Otóż zamiast tego powinno być jeszcze
 #    (sub)if-owanie na temat odpowiednika kolczyrazu, tyle że do spraw nie wyświetlania, a zapisu
 # sęk w tym, że będzie to miało sens dopiero, gdy powstanie zapis
 else:
-	print 'kolczyraz nie jest ani k, ani r, ani n, więc czym jest?!'
+	print 'kolczyraz nie jest ani k, ani r, ani n, więc czym jest?! Przecież ten program nie może, ot tak, niczego nie robić!'
 	try:
 		print 'Otóż jest on stringiem "%s"' % str(kolczyraz)
 	except:
