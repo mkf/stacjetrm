bd0ff5ed719ed26b811ce3ed20c6486c8c191a42
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-08-05 14:16:51 +0200

    Zapomniano o wyborze regułowym określonej metody orzeczima, który powinien się był znaleźć w pokaz.py. Skopiowano jak na razie starocia z zapetlonego, ale trza będzie zmodernizować do nowych parametrów

--- pokaz.py
+++ pokaz.py
@@ -8,4 +8,23 @@ class pokaz:
 		from orzeczim import orzeczim
 		#if stac #dokonczyc watek myslowy
 		orze = orzeczim(row,int(stac),unix,pracy,lang,jezadr,lanchar)
-	
+		if pracy == 'u':
+			orze.orz()
+			orze.pisul()
+		elif pracy == 'l':
+			orze.orz()                                                  
+			orze.pisul()
+		elif pracy == 'p':
+			orze.pisp()
+		elif pracy == 'c':
+			orze.pisc()
+		elif pracy == 'm':
+			orze.pism()
+		elif pracy == 'a':
+			orze.orz()
+			orze.pisa()
+		elif pracy == 't':
+			orze.pist()
+		else:
+			print "Elektu labormodo a, l, u, p, c aux m se vi volas ke tiun programon funkcas."
+				print "Plej eble ke la labormodo ", pracy, " estas malbona."
