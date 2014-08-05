b5286433cfbbeb9adec7e702f8f90501eacaf066
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-08-05 13:57:04 +0200

    tymczasowo zakomentowany 'wątek myślowy' ifa w pokaz

--- pokaz.py
+++ pokaz.py
@@ -6,6 +6,6 @@ class pokaz:
 	
 	def __init__(self,row,stac,unix,pracy,lang,jezadr,lanchar):
 		from orzeczim import *
-		if stac #dokonczyc watek myslowy
+		#if stac #dokonczyc watek myslowy
 		orze = orzeczim(row,sn,unix,pracy,lang,jezadr,lanchar)
 	
