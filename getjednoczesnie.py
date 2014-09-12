8aefd3ae9a9852448f09e3aa3f01dcfcf6211a62
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-09-12 10:36:48 +0200

    Rozpoczęcie właściwe prac nad getjednoczesnie

--- getjednoczesnie.py
+++ getjednoczesnie.py
@@ -4,8 +4,23 @@ class getjednoczesnie:
 	from download import * 
 #	import time
 	from pars import *
+import Queue
+import threading
 
 	def __init__(self,stacje,pracy,lang,jezadr,lanchar):
-		
-	def pobierz(self):
+		sa = 0
+		ssa = 0
+		sw = 0
+		ssw = 0
+		stacdict = {}
+		slownikczasow = ()
+		q = Queue.Queue()
+		for s in stacje:
+			sa = 0
+			sw = 0
+			a = 
+			t = threading.Thread(target=self.pobierzjedno, agrs = (q,s))
+			t.daemon=True
+			t.start()
+	def pobierzjedno(self, q, s):
 		
