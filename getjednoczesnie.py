59ae407c5a39945406fdd68f1113602e2a5cdd26
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-09-13 14:50:25 +0200

    Prace nad getjednoczesnie

--- getjednoczesnie.py
+++ getjednoczesnie.py
@@ -7,7 +7,10 @@ class getjednoczesnie:
 import Queue
 import threading
 
-	def __init__(self,stacje,pracy,lang,jezadr,lanchar):
+	def __init__(self,stacje,pracy,lan,jezadr,lanchar):
+		self.lan = lan
+		self.jezadr = jezadr
+		self.lanchar = lanchar
 		sa = 0
 		ssa = 0
 		sw = 0
@@ -22,6 +25,48 @@ import threading
 			t = threading.Thread(target=self.pobierzjedno, agrs = (q,s))
 			t.daemon=True
 			t.start()
-	def pobierzjedno(self, q, s):
-		
+	
+
+	def suma(self
+
+	def si(self,s):
+		lan = self.lan
+		sa = 0
+		sw = 0
+		if int(s) == 0:
+			sa = 1
+			a = 'a'
+			return a
+		elif int(s) == 100:
+			sw = 1
+			a = 'w'
+			return a
+		elif int(s) < 0:
+			o = int(s) * (-1)
+			a = self.si(o)
+			return a
+		elif int(s) < 13:
+			from download import download
+			thisisthedownloadinstance = download(int(s),lan.dictu())
+			a = thisisthedownloadinstance.raz()
+			return a
+		else:
+			jezodict = lan.dictu()
+			print "%s: %s" % jezodict[badstacparam],str(s)
+			quit()			
+			
+	def praca(self,st,row,utim,pr):
+		lan = self.lan
+		jezadr = self.jezadr
+		lanchar = self.lanchar
+		from pokaz import pokaz
+		if pr == 'n':
+			pracowanie='nie'
+#		elif pr == 'f' or pr == 'l' or pr == 'u' or pr == 'a' or pr == 't' or pr == 'k' or pr == 'c' or pr == 'm':
+		else:
+			pokaz(row,st,utim,pr,lan,jezadr,lanchar)
+	def slowstac(self):
+		return self.stacdict
+	def slowczas(self):
+		return self.slwcza
 # http://stackoverflow.com/questions/2846653/python-multithreading-for-dummies
