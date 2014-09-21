24cc199733b3bc27f455f3f7cd95e68c96586b18
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-09-21 14:08:51 +0200

    Dalsze próby wyrównania poprawnego spacji i tabulacji, poprawka literówki.

--- polski.py
+++ polski.py
@@ -30,20 +30,20 @@ class pl_natio:
 		elif type(row) == int and row <= 0:
 			wyd = u"najprawdopodobniej wystąpił błąd - stacja twierdzi, że jest tam " + str(row) + " rowerów - a jest to liczba ujemna \n		  "
 		elif type(row) == int and row == 1:
-			wyd = u"jest    1  rower	"
+			wyd = u"jest	 1  rower	"
 		elif type(row) == int and row >= 2:
 			if row < 4 or row == 4:
-				wyd = (u"są      " + str(row) + u"  rowery   ")
+				wyd = (u"są  	 " + str(row) + u"  rowery   ")
 			elif row > 5 or row == 5:
-				wyd = (u"jest    " + str(row) + u"  rowerów  ")
+				wyd = (u"jest	 " + str(row) + u"  rowerów  ")
 			elif row > 10 or row == 10:
-				wyd = (u"jest " + str(row) + u"  rowerów   ")
+				wyd = (u"jest	" + str(row) + u"  rowerów   ")
 			elif row > 22 or row == 22:
-				wyd = (u"są   " + str(row) + u"  rowery, choć to praktycznie niemożliwe, gdyż stacja tyle nie pomieści")
+				wyd = (u"są  	" + str(row) + u"  rowery, choć to praktycznie niemożliwe, gdyż stacja tyle nie pomieści")
 		elif row == "Download failed":
 			wyd = u", nie wiemy ile rowerów jest na tej stacji, ponieważ pobieranie zakończyło się błędem"
 		else:
-			wyd = u", nie wiemy ile rowerów jest na tej stacji, ponieważ próba zdobycia informacji zakończyła się błędem, zwróciłą [typ: '%s', wartość: '%s']" % (type(row), str(row))
+			wyd = u", nie wiemy ile rowerów jest na tej stacji, ponieważ próba zdobycia informacji zakończyła się błędem, zwróciła [typ: '%s', wartość: '%s']" % (type(row), str(row))
 		return wyd
 	def lanstac(self):
 		lanstadict = {
