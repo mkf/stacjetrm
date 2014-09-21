71dbd55f2e052736e7b4e640132e4b5138eff9e5
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-09-21 06:35:21 +0200

    Zmieniono nazwę na jednoczenie, m.in. w celu skorelowania z mapą myślową

--- dajrazem.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-class dajrazem:
-	"To jest klasa do dawania liczby rowerow na wszystkich stacjach naraz w formie slownika"
-	
-	def __init__(self,pracy,debugu,unix,dzien,czas,trwania):
-		
